e8f106d35d5c292e636bfa9f65c750972ac053cd
Vi Tran <Vi.n.tran.23@dartmouth.edu>
2021-01-16 15:36:15 -0800

    ohtml for character selection

--- app.js
+++ app.js
@@ -11,6 +11,16 @@ app.get("/", (_req, res) => {
     res.render("selection", {dummyVariable: "diff"});
 })
 
+let motherList = [
+    {name:"Lorraine", budget:"140k", maritalStatus:"Married",numChildren:"1",occupation:"Coporate",education:"College",parentsLoc:"Out-of-state"},
+    {name:"Riley", budget:"85k", maritalStatus:"Married",numChildren:"3",occupation:"Ph. Therapist",education:"College",parentsLoc:"Close by"},
+    {name:"Ashley", budget:"30k", maritalStatus:"Single",numChildren:"1",occupation:"Receptionist",education:"High School",parentsLoc:"Out-of-state"},
+    {name:"Catherine", budget:"15k", maritalStatus:"Married",numChildren:"1",occupation:"Cashier",education:"Middle School",parentsLoc:"Out-of-state"},
+]
+app.get("/characterPage", (_req, res) => {
+    res.render("characterPage", {momList: motherList});
+})
+
 app.post("/", (req, _res) => {
     console.log(req.body.someVariable);
 })
