c80dbf6299482742da27e7738661b595fd4739be
Angela Zhang <angela.zhang@dali.dartmouth.edu>
2021-01-17 13:46:54 -0800

    merge: barkin & vi's code into angela's code

--- public/js/frontendfunctions.js
+++ public/js/frontendfunctions.js
@@ -2,19 +2,12 @@ var modal = document.getElementById("splash");
 var span = document.getElementsByClassName("close")[0];
 
 
-
-function fadeOutEffect() {
-    var fadeTarget = modal;
-    var fadeEffect = setInterval(function () {
-        if (!fadeTarget.style.opacity) {
-            fadeTarget.style.opacity = 1;
-        }
-        if (fadeTarget.style.opacity > 0) {
-            fadeTarget.style.opacity -= 0.1;
-        } else {
-            clearInterval(fadeEffect);
-        }
-    }, 25);
+span.onclick = function() {
+    modal.style.display = "none";
 }
 
-span.addEventListener('click', fadeOutEffect);
+window.onclick = function(event) {
+    if (event.target = modal) {
+        modal.style.display = "none";
+    }
+}
